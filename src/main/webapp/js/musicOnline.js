7465fcaffbe8bb72123e8e7365ddd79d3a4a9644
Stefan Blixt <Stefan.Blixt@cybercom.com>
2014-03-15 12:43:52 +0100

    Stefan lade till soundcloud

--- src/main/webapp/js/musicOnline.js
+++ src/main/webapp/js/musicOnline.js
@@ -1,6 +1,9 @@
-$(function() {
+function setupMusicOnline() {
+    SC.get('/tracks', { q: 'Beardyman'}, function(tracks) {
+        for (var i = 0; i < tracks.length; i++) {
+            var item = tracks[i];
+            $("#soundCloudItems").append('<div>' + item.user.username + " / " + item.title + '</div>');
+        }
+    });
+}
 
-    $('#music').on('show', function() {
-        console.log("Hello");
-    })
-});
