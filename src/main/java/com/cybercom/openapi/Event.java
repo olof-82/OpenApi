07fbc73264a899c6b3812ba1cdd0846938896bcb
Olof Persson <olof.persson@cybercom.com>
2014-03-15 13:38:28 +0100

    Updated

---
+++ src/main/java/com/cybercom/openapi/Event.java
@@ -0,0 +1,80 @@
+package com.cybercom.openapi;
+
+/**
+ *
+ * @author oope
+ */
+public class Event {
+    
+    private String eventId;
+    
+    private String artist;
+
+    private String title;
+
+    private String city;
+
+    private String country;
+    
+    private String street;
+    
+    private String startDate;
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(String eventId) {
+        this.eventId = eventId;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+    
+    
+}
