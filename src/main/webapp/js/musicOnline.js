67c0fcabfdca3e371e81b247fb31165085d60a29
Stefan Blixt <Stefan.Blixt@cybercom.com>
2014-03-15 14:07:17 +0100

    Stefan lade till youtube stuff

--- src/main/webapp/js/musicOnline.js
+++ src/main/webapp/js/musicOnline.js
@@ -1,16 +1,40 @@
-function appendTrack(track) {
+function appendSoundCloudTrack(track) {
     var divId = "soundcloud" + track.id;
     $("#soundCloudItems").append('<div id="' + divId + '"/>');
     SC.oEmbed(track.uri, $('#' + divId).get(0));
 }
 
-function setupMusicOnline() {
+function setupSoundCloudMusicOnline() {
     SC.get('/tracks', { q: 'Beardyman'}, function(tracks) {
         for (var i = 0; i < Math.min(3, tracks.length); i++) {
             var item = tracks[i];
 //            $("#soundCloudItems").append('<div>' + item.user.username + " / " + item.title + '</div>');
-            appendTrack(item);
+            appendSoundCloudTrack(item);
         }
     });
+}
+
+function appendYouTubeTrack(youTubeId) {
+    var divId = "jutubtrack-" + youTubeId;
+    $("#youTubeItems").append('<div id="' + divId + '"/>');
+    $("#" + divId).html('<iframe id="ytplayer" type="text/html" width="640" height="360" src="https://www.youtube.com/embed/' + youTubeId + '" frameborder="0" allowfullscreen>');
+}
+
+function setupYouTubeMusicOnline() {
+    $.get('http://gdata.youtube.com/feeds/api/videos',
+        {q: 'Beardyman'}, function(xml) {
+            var ids = $(xml).find('entry id');
+            for (var i = 0; i < Math.min(3, ids.length); i++) {
+                var id = ids[i];
+                var youTubeId = id.textContent.substring(id.textContent.lastIndexOf('/') + 1);
+                appendYouTubeTrack(youTubeId);
+            }
+        });
+//    appendYouTubeTrack();
+}
+
+function setupMusicOnline() {
+    setupYouTubeMusicOnline();
+    setupSoundCloudMusicOnline();
 }
 
