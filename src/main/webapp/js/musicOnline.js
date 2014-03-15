0bac58ce9e34e1576e117f619af4ca7fc8c88185
Stefan Blixt <Stefan.Blixt@cybercom.com>
2014-03-15 13:09:36 +0100

    Stefan lade till soundcloud widgets

--- src/main/webapp/js/musicOnline.js
+++ src/main/webapp/js/musicOnline.js
@@ -1,8 +1,15 @@
+function appendTrack(track) {
+    var divId = "soundcloud" + track.id;
+    $("#soundCloudItems").append('<div id="' + divId + '"/>');
+    SC.oEmbed(track.uri, $('#' + divId).get(0));
+}
+
 function setupMusicOnline() {
     SC.get('/tracks', { q: 'Beardyman'}, function(tracks) {
-        for (var i = 0; i < tracks.length; i++) {
+        for (var i = 0; i < Math.min(3, tracks.length); i++) {
             var item = tracks[i];
-            $("#soundCloudItems").append('<div>' + item.user.username + " / " + item.title + '</div>');
+//            $("#soundCloudItems").append('<div>' + item.user.username + " / " + item.title + '</div>');
+            appendTrack(item);
         }
     });
 }
